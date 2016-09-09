522987e968bc62af33c06ca9d1b3aa64f5295e78
Alex Arslan <ararslan@comcast.net>
2016-09-08 23:08:31 -0700

    Corrected build directory name

--- deps/build.jl
+++ deps/build.jl
@@ -26,8 +26,8 @@ else
     provides(BuildProcess, (@build_steps begin
         GetSources(zstd)
         @build_steps begin
-            ChangeDirectory(joinpath(srcdir(zstd), "openspecfun-$vers"))
-            FileRule(joinpath(libdir(zstd), "libopenspecfun." * BinDeps.shlib_ext), @build_steps begin
+            ChangeDirectory(joinpath(srcdir(zstd), "zstd-$vers"))
+            FileRule(joinpath(libdir(zstd), "libzstd." * BinDeps.shlib_ext), @build_steps begin
                 CreateDirectory(libdir(zstd))
                 `make install PREFIX=$(BinDeps.usrdir(zstd))`
             end)

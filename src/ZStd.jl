f63bd7da6a3b5a3f6dc8d32951460cc21e14554f
Alex Arslan <ararslan@comcast.net>
2016-09-08 22:55:03 -0700

    Add a couple basic helper functions

--- src/ZStd.jl
+++ src/ZStd.jl
@@ -9,4 +9,31 @@ else
           "and restart Julia.")
 end
 
+
+immutable ZStdError <: Exception
+    msg::String
+end
+
+Base.showerror(io::IO, ex::ZStdError) = print(io, "ZStd: " * ex.msg)
+
+
+# Determine whether the input represents a zstd error, yes => throw it, no => return it
+function check_zstd_error(code::Csize_t)
+    iserr = Bool(ccall((:ZSTD_isError, :libzstd), Cuint, (Csize_t, ), code))
+    if iserr
+        msg = unsafe_string(ccall((:ZSTD_getErrorName, :libzstd), Ptr{Cchar}, (Csize_t, ), code))
+        throw(ZStdError(msg))
+    end
+    return code # input is not an error
+end
+
+
+"""
+    ZStd.MAX_COMPRESSION
+
+An integer representing the maximum compression level available.
+"""
+const MAX_COMPRESSION = Int(ccall((:ZSTD_maxCLevel, :libzstd), Cint, ()))
+
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,10 @@
 using ZStd
 using Base.Test
 
-# TODO
+@testset "helpers" begin
+    @test typeof(ZStd.MAX_COMPRESSION) == Int
+    @test ZStd.MAX_COMPRESSION > 0
+
+    @test ZStd.check_zstd_error(UInt64(0)) == UInt64(0)
+    @test_throws ZStd.ZStdError ZStd.check_zstd_error(typemax(UInt64))
+end
